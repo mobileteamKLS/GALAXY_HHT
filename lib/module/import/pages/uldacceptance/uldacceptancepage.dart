920f2b16b33aff7b05c4735a94a7bd4977e8f61f
Yash Parmar <parmar.yash1995@gmail.com>
2024-10-28 23:22:13 +0530

    bug fix for weight formate

--- lib/module/import/pages/uldacceptance/uldacceptancepage.dart
+++ lib/module/import/pages/uldacceptance/uldacceptancepage.dart
@@ -3541,7 +3541,7 @@ class _UldAcceptancePageState extends State<UldAcceptancePage> with SingleTicker
       if(specialCharAllow == true){
         SnackbarUtil.showSnackbar(context, "Only alphanumeric characters are accepted.", MyColor.colorRed, icon: FontAwesomeIcons.times);
         Vibration.vibrate(duration: 500);
-        locationController.clear();
+        uldNoController.clear();
 
 
         WidgetsBinding.instance.addPostFrameCallback((_) {
